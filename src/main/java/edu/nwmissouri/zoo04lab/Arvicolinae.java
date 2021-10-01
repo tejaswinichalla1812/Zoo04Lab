b341b9c0d38562113b8725442d398815cd6410dc
nikhil-akarapu <37258852+nikhil-akarapu@users.noreply.github.com>
2021-10-01 11:50:55 -0500

    Create Arvicolinae.java

---
+++ src/main/java/edu/nwmissouri/zoo04lab/Arvicolinae.java
@@ -0,0 +1,21 @@
+package edu.nwmissouri.Akarapu04lab;
+/**
+ *
+ * @author NIKHIL KUMAR AKARAPU
+ */
+public class Arvicolinae extends Animal{
+    /* Arvicolinae constructor
+    *@param name- the name of this Arvicolinae
+    */
+    public Arvicolinae(String name){
+        super(name);
+    }
+    @Override
+    public void speak() {
+        System.out.println("I'm an Arvicolinae!");
+    }
+    @Override
+    public void move() {
+        System.out.println("When I move, I walk, walk, walk.");
+    } 
+}
