a1722ad0e60773a3d803f4dde41bb864d24fc7bb
Harika Chintala <dimpulchintala@gmail.com>
2021-10-01 11:49:50 -0500

    Create Cow.java

---
+++ src/main/java/edu/nwmissouri/zoo04lab/Cow.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.nwmissouri.HarikaChintala04lab;
+
+/**
+ *
+ * @author Harika_Chintala
+ */
+public class Cow extends Animal{
+    
+    /**
+     * Cow Constructor
+     * @param name - the name of this cow
+     */
+    public Cow(String name){
+        super(name);
+    }
+     @Override
+    public void eat(){
+        System.out.println(" I eat like Yumm yumm yumm!!");
+    }
+    @Override
+    public void speak(){
+        System.out.println(" I am a Cow.. "+this.name);
+    }
+    @Override
+    public void child(){
+        System.out.println(" I can give birth to one child at once");
+    }
+     @Override
+    public void color(){
+        System.out.println("I can be in black, brown and white color ");
+    }
+}
