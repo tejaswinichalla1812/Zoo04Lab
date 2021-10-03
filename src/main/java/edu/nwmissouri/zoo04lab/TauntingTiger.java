bf8aabb8531be3dc68202666f73af590cf8f0c84
SARATH ARREPU <89147362+SarathArrepu@users.noreply.github.com>
2021-10-03 10:49:31 -0500

    updated code

--- src/main/java/edu/nwmissouri/zoo04lab/TauntingTiger.java
+++ src/main/java/edu/nwmissouri/zoo04lab/TauntingTiger.java
@@ -1,1 +1,50 @@
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.nwmissouri.zoo04lab;
+
+/**
+ *
+ * @author Sarath Arrepu
+ */
+public class TauntingTiger extends Animal {
+    /**
+     * This is the parametrized constructor
+     * @param name
+     */
+    public TauntingTiger(String name) {
+        super(name);
+    }
+    /**
+     * This method is about the animal speaking
+     */
+    @Override
+    public void speak() {
+        System.out.println("I roar");
+    }
+    /**
+     * This method is about the animal movement
+     */
+    @Override
+    public void move() {
+        System.out.println("I hunt");
+    }
+    /**
+     * This method is about the animal eating
+     */
+    @Override
+    public void eat() {
+        System.out.println("I eat meat");
+    }
+    /**
+     * This method is about the animal type
+     */
+     public void type() {
+        System.out.println("I am a wild animal");
+    }
+    
+    
+}
