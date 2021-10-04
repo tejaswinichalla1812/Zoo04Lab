4cadee0b4d6a4ace0cb970de2fa6f85f81131b54
Denise Case <denisecase@gmail.com>
2021-10-03 19:12:02 -0500

    Add code to auto-build switch as Groups are created

---
+++ src/main/java/edu/nwmissouri/zoo04lab/ZooBuildSwitch.java
@@ -0,0 +1,84 @@
+package edu.nwmissouri.zoo04lab;
+
+import static edu.nwmissouri.zoo04lab.ZooCheckFiles.getFileLines;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+/**
+ * Build our switch statement so we can see all the animals.
+ *
+ * @author Denise Case
+ */
+public class ZooBuildSwitch {
+
+    private static final String relativePathToPackage = "/src/main/java/edu/nwmissouri/zoo04lab";
+    private static final String nonAnimalsFileString = "SOURCE_NON_ANIMAL_FILES.txt";
+
+    public static void main(String args[]) throws IOException {
+
+        // find the files that should be excluded
+        ArrayList<String> ignoreList = getNonCustomAnimalFiles();
+        System.out.println("Ignore these:");
+        ignoreList.forEach(f -> System.out.println(f));
+
+        // process all found files, outputing custom animal code
+        System.out.println("From all these in the package:");
+        File fileFolder = new File(getCustomAnimalPackagePathString());
+        String filesArray[] = fileFolder.list();
+        for (String s : filesArray) {
+            System.out.println(s);
+        }
+
+        System.out.println("===============================");
+        System.out.println("Custom Animal Files in Package");
+        System.out.println("===============================");
+        int n = 1;
+        for (String file : filesArray) {
+            if (!ignoreList.contains(file)) {
+                int fileLength = file.length();
+                int lengthExtension = ".java".length();
+                int fileNameLength = fileLength - lengthExtension;
+                var justName = file.substring(0, fileNameLength);
+                if (justName.endsWith("Group")) {
+                    
+                    // output this:
+                    //case 1 -> {             
+                    //    AardvarkGroup.create();
+                    //    AardvarkGroup.run();
+                    //}
+                    System.out.println("case " + n++ + " -> {");
+                    System.out.println(justName + ".create();");
+                    System.out.println(justName + ".run();");
+                    System.out.println("}");
+                }
+            }
+        }
+        System.out.println("===============================\n");
+
+    }
+
+    /**
+     * Get a list of expected files in the root project directory.
+     *
+     * @return String[] of expected file names
+     */
+    private static ArrayList<String> getNonCustomAnimalFiles() {
+        return getFileLines(nonAnimalsFileString);
+    }
+
+    /**
+     * Get our project package path as a String.
+     *
+     * @return project package String
+     */
+    private static String getCustomAnimalPackagePathString() {
+        Path projectPath = Paths.get("").toAbsolutePath();
+        String projectPathString = projectPath.normalize().toString() + relativePathToPackage;
+        System.out.println(projectPathString);
+        return projectPathString;
+    }
+
+}
