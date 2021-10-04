c327e9f96602f83ae5ce057bfc5a624c92b3d70d
Sake Maneesh <maneeshsake99@gmail.com>
2021-10-04 18:27:26 -0500

    added Cat.java

--- src/main/java/edu/nwmissouri/zoo04lab/Cat.java
+++ src/main/java/edu/nwmissouri/zoo04lab/Cat.java
@@ -1,23 +1,26 @@
-package edu.nwmissouri.Zoo04lab;
+
+import edu.nwmissouri.zoo04lab.Animal;
+
 
 /**
  *
  * @author Maneesh7
  */
+public class Cat extends Animal {
 
-    public class Cat extends Animal{
-    
-    public Cat(String name){
+    public Cat(String name) {
         super(name);
-   
+
     }
+
     @Override
-    public void speak(){
-        System.out.println(" I am a Cat.. "+this.name);
+    public void speak() {
+        System.out.println(" I am a Cat.. " + this.name);
     }
+
     @Override
-    public void move(){
+    public void move() {
         System.out.println(" I can run faster");
-    
+
     }
 }
