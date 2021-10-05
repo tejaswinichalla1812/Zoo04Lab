ecd937abe1e1658cd5b4cbfa098b8119f069f6a4
Satheesh Eppalapelli <s545243@nwmissouri.edu>
2021-10-05 12:45:49 -0500

    Create FloppyTest.java

---
+++ src/test/java/edu/nwmissouri/zoo04lab/FloppyTest.java
@@ -0,0 +1,71 @@
+package edu.nwmissouri.zoo04lab;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test Floppy using JUnit5 
+ *
+ * @author S545243  Satheesh Eppalapelli
+ */
+public class FloppyTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    public FloppyTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of speak method, of class Floppy.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testSpeak() throws Exception {
+        String expected = "I'm Allie. I'm an Floppy!";
+        var aardvark = new Floppy("Allie");
+        aardvark.speak();
+        String actual = outputStreamCaptor.toString().trim();
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test of move method, of class Floppy.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testMove() throws Exception {
+        String expected = "When I move, I walk, walk, walk.";
+        var aardvark = new Floppy("Allie");
+         aardvark.move();
+        String actual = outputStreamCaptor.toString().trim();
+        assertEquals(expected, actual);
+    }
+
+}
