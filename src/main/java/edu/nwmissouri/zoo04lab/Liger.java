b3e923c66e099a6084173a27d1f4d258e9d2a7eb
revanthbabu99 <89222081+revanthbabu99@users.noreply.github.com>
2021-10-18 22:52:11 -0500

    Updated files

--- src/main/java/edu/nwmissouri/zoo04lab/Liger.java
+++ src/main/java/edu/nwmissouri/zoo04lab/Liger.java
@@ -26,9 +26,41 @@ public class Liger extends Animal {
 
     @Override
     public void speak() {
-         System.out.printf("I'm %s. I'm an Liger! \n", this.name);
+         System.out.printf("I roar very loudly! \n", this.name);
     }
     
     
+    public void eat() {
+         System.out.printf("I hunt very badly \n", this.name);
+    }
+     public void profess() {
+        double a = 7.5;
+        int b = 2;
+        double c = getLigerAddition(a, b);
+        System.out.printf("I know LigerAddition! %4.2f plus %d is %4.2f \n", a, b, c);
+    }
+
+    /**
+     * Custom Liger function - your class must use a unique function name
+     * unique first parameter identifier unique second parameter identifier do
+     * something creative
+     *
+     * @param valueOne double input
+     * @param valueTwo int input
+     * @return double sum
+     */
+    public double getLigerAddition(double valueOne, int valueTwo) {
+        return valueOne + valueTwo;
+    }
+
+    public static void main(String[] args) {
+        var a = new Liger("Tester");
+        a.speak();
+        a.move();
+        a.profess();
+        a.eat();
+    }
+
 }
+
 
