5dc519dfc66ab976a308d1611efc31736e44efab
Gayatri Devabhaktuni <89164709+gayatridevabhaktuni@users.noreply.github.com>
2021-10-18 22:37:14 -0500

    Update Shoebill.java

--- src/main/java/edu/nwmissouri/zoo04lab/Shoebill.java
+++ src/main/java/edu/nwmissouri/zoo04lab/Shoebill.java
@@ -28,6 +28,13 @@ public class Shoebill extends Animal {
     public void eat() {
         System.out.println("I Love eating Fish");
     }
+    
+     public void confess() {
+        double a = 3.7;
+        int b = 5;
+        double c = getShoebillAddition(a, b);
+        System.out.printf("I know ShoebillAddition! %4.2f plus %d is %4.2f \n", a, b, c);
+    }
     public static void main(String[] args){
         var sb = new Shoebill("Gayatri");
         sb.speak();
