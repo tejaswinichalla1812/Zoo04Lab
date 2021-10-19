ecd341c311a55c3e05a3e767843b713c02b1a248
preethi54 <je.preethi@gmail.com>
2021-10-18 19:17:55 -0500

    updated Panda.java , PandaGroup.java and respective test files PandaTest.java,PandaGroupTest.java

--- src/main/java/edu/nwmissouri/zoo04lab/Panda.java
+++ src/main/java/edu/nwmissouri/zoo04lab/Panda.java
@@ -21,17 +21,52 @@ public class Panda extends Animal {
         super(name);
     }
 
-
+    /**
+     * speak overrides function in animal class
+     */
     @Override
     public void speak() {
         System.out.println("Hi I am a " + this.name + "! I Squeak.. ");
     }
-
+    /**
+     * move overrides function in animal class
+     */
     @Override
     public void move() {
         System.out.println("I am a lazy animal and love to roll on ground");
     }
-    
+    /**
+     * function pandaType
+     */
+    public void pandaType(){
+        String firstName = "RED";
+        String lastName = "PANDA";
+        
+        System.out.println("There are 2 types of panda, RED panda ,BLACK-WHITE pandas....");
+        String fullName = fullname(firstName,lastName);
+       System.out.println("My full name is : "+fullName);
+
+    }
+    /**
+     * 
+     * @param fname
+     * @param lname
+     * @return 
+     */
+        public String fullname(String fname,String lname){
+        return fname+lname;
+    }
+        /**
+         * 
+         * @param args 
+         * main class for panda 
+         */
+        public static void main(String[] args) {
+        var panda = new Panda("Test");
+        panda.speak();
+        panda.move();
+        panda.pandaType();
+    }
  /*    @Override
      public void looks() {
          System.out.println("Hi I am a " + this.name + "! i am black and white with fur..");
