df415aed675e8aa820fb4236f6bd0db247d4ab87
Hemanth Sai Reddy Velamuri <89224106+hemanthsaireddyvelamuri@users.noreply.github.com>
2021-10-19 00:06:06 -0500

    Update BlackPanther.java

--- src/main/java/edu/nwmissouri/zoo04lab/BlackPanther.java
+++ src/main/java/edu/nwmissouri/zoo04lab/BlackPanther.java
@@ -21,7 +21,7 @@ public class BlackPanther extends Animal {
      */
     @Override
     public void speak() {
-        System.out.printf("I'm %s. I am a BlackPanther!",this.name);
+        System.out.printf("I'm %s. I'm an BlackPanther!",this.name);
     }
     
     /**
@@ -31,5 +31,23 @@ public class BlackPanther extends Animal {
     @Override
     public void move() {
         System.out.println("When I move, I run, run, run.\n");
+    }
+    
+    public void profes(){
+        double a = 2.5;
+        int b = 2;
+        double c = getBlackPantherAddition(a,b);
+        System.out.printf("I know BlackPanther addition! %4.2f plus %d is %4.2f \n",a,b,c);
+    }
+
+    public double getBlackPantherAddition(double first, int second) {
+        return first+second; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public static void main(String[] args) {
+        var a = new BlackPanther("Tester");
+        a.speak();
+        a.move();
+        a.profes();
     }
 }
