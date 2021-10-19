e0c6a33e6c23866bf8887912c358fe3eed763680
SARATH ARREPU <89147362+SarathArrepu@users.noreply.github.com>
2021-10-19 16:02:10 -0500

    java doc

--- src/main/java/edu/nwmissouri/zoo04lab/TauntingTigerGroup.java
+++ src/main/java/edu/nwmissouri/zoo04lab/TauntingTigerGroup.java
@@ -3,7 +3,7 @@ package edu.nwmissouri.zoo04lab;
 import java.util.ArrayList;
 
 /**
- *
+ * Provides methods to create and run a group of TauntingTiger
  * @author Sarath Arrepu (S545027)
  */
 public class TauntingTigerGroup {
