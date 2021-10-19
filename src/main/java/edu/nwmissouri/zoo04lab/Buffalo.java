0d6fcb8ec1f4fa14fa2f666ed99e18a9ba6fe5f4
Bhaskar Venkata Prudhvi Bobbala <89476750+Baskar0106@users.noreply.github.com>
2021-10-18 20:29:31 -0500

    Update Buffalo.java

--- src/main/java/edu/nwmissouri/zoo04lab/Buffalo.java
+++ src/main/java/edu/nwmissouri/zoo04lab/Buffalo.java
@@ -7,9 +7,15 @@ package edu.nwmissouri.zoo04lab;
 
 /**
  *
- * @author Bhaskar
+ * @author Bhaskar Venkata Prudhvi Bobbala
  */
 public class Buffalo extends Animal {
+    enum COLOR
+            {
+        BLACK,
+        BROWN
+    }
+
 
     /**
      * *
@@ -38,5 +44,67 @@ public class Buffalo extends Animal {
     public void move() {
         System.out.println("I usually walk with four legs");
     }
+    /**
+     * *
+     * Override method for buffalo diet
+     */
+    //@Override
+    public void diet() {
+        System.out.println("I'm a Herbivore");
+    }
+
+    /**
+     * *
+     * Override method for buffalo family
+     */
+    //@Override
+    public void family() {
+        System.out.println("I am from Bovidae family");
+    }
+
+    /**
+     * *
+     * Override method for buffalo lifespan
+     */
+    //@Override
+    public void lifespan() {
+        System.out.println("My life span is 10-20 years");
+    }
+    
+     /**
+     * 
+     * @param morningMilkProduction is of type double 
+     * @param eveningMilkProduction is of type double
+     * @return double value
+     */
+    public double getBuffaloAddition(double morningMilkProduction, double eveningMilkProduction)
+    {
+        return morningMilkProduction + eveningMilkProduction;
+    }
+    
+    /**
+     * 
+     */
+    public void Buffalo(){
+        double morningMilkProduction = 4.5;
+        double eveningMilkProduction = 4.0;
+        double perDayMilkProduction = getBuffaloAddition(morningMilkProduction, eveningMilkProduction);
+        System.out.println("I am " + this.name + ", I produce " + morningMilkProduction + "liters Of milk in the morning and "+eveningMilkProduction+"liters in the Evening, Overall I will produce "+perDayMilkProduction+"\"liters per\" day");
+    }
+    
+    public static void main(String[] args){
+        Buffalo Buffalo=new Buffalo("Bobbala");
+        Buffalo.speak();
+        Buffalo.move();
+        Buffalo.diet();
+        Buffalo.family();
+        Buffalo.lifespan();
+        COLOR C1=COLOR.BLACK;
+        COLOR C2=COLOR.BROWN;
+        System.out.println("enums: "+C1+"\n"+C2);
+    }
+        
+        
 
 }
+
