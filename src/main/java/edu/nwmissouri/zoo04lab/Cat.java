179ff6d2fc20a33963561ea14b807415584015c6
ManeeshSake <89217109+ManeeshSake@users.noreply.github.com>
2021-10-18 22:42:37 -0500

    Update Cat.java

--- src/main/java/edu/nwmissouri/zoo04lab/Cat.java
+++ src/main/java/edu/nwmissouri/zoo04lab/Cat.java
@@ -1,4 +1,3 @@
-
 package edu.nwmissouri.zoo04lab;
 
 
@@ -23,4 +22,34 @@ public class Cat extends Animal {
         System.out.println(" I can run faster");
 
     }
+    
+    public void profess() {
+        double a = 2.5;
+        int b = 2;
+        double c = getCatAddition(a, b);
+        System.out.printf("I know AardvarkAddition! %4.2f plus %d is %4.2f \n", a, b, c);
+    }
+
+    /**
+     * Custom Aardvark function - your class must use a unique function name
+     * unique first parameter identifier unique second parameter identifier do
+     * something creative
+     *
+     * @param valueOne double input
+     * @param valueTwo int input
+     * @return double sum
+     */
+     public double getCatAddition(double valueOne, int valueTwo) {
+        return valueOne + valueTwo;
+    }
+
+    public static void main(String[] args) {
+        var a = new Cat("Tester");
+        a.speak();
+        a.move();
+        a.profess();
+    }
+
 }
+
+    
